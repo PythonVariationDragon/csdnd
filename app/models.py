06ad51968e99dd43ba5fcee9222214c219281d23
nanfengpo <lsxxxxxx@126.com>
2017-09-02 15:28:30 +0800

    测试更改

--- app/models.py
+++ app/models.py
@@ -18,6 +18,8 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64))
 
+#。。。
+
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
